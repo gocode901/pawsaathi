dd44d9be0e930c383d28bd6921971e734aa11e81
SomnathAmbati <somnath.ambati@gmail.com>
2024-08-07 10:19:39 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Signup from "./pages/signup";
+import Signup1 from "./pages/Signup1";
+import Signup3 from "./pages/Signup3";
 
 function App() {
-  <Signup></Signup>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup1 />} />
+        <Route path="/signup3" element={<Signup3 />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
